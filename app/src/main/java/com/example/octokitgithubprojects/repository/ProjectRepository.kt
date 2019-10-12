9c9c4bd9acf94249e016c528428e983f9ccb0520
Minami Munakata <june28munazo@gmail.com>
2019-10-13 00:21:57 +0900

    Create a repository, the data provider for ViewModel, to interact with GitHub service.

---
+++ app/src/main/java/com/example/octokitgithubprojects/repository/ProjectRepository.kt
@@ -0,0 +1,30 @@
+package com.example.octokitgithubprojects.repository
+
+import com.example.octokitgithubprojects.model.Project
+import com.example.octokitgithubprojects.service.GitHubService
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val HTTPS_API_GITHUB_URL = "https://api.github.com/"
+
+class ProjectRepository {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(HTTPS_API_GITHUB_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private var gitHubService: GitHubService = retrofit.create(GitHubService::class.java)
+
+    // HTTP request returns the list of repositories.
+    suspend fun getProjectList(userId: String): Response<List<Project>> = gitHubService.getProjectList(userId)
+
+    // Return the singleton ProjectRepository instance.
+    companion object Factory {
+        val instance: ProjectRepository
+        @Synchronized get() {
+            return ProjectRepository()
+        }
+    }
+}
