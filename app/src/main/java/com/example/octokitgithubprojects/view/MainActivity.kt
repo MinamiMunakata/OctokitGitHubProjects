69649b77e2b5264b8808bb04d0d48e6b0b065184
Minami Munakata <june28munazo@gmail.com>
2019-10-13 14:52:37 +0900

    Create ProjectList Fragment with xml. Add RecyclerView.

--- app/src/main/java/com/example/octokitgithubprojects/view/MainActivity.kt
+++ app/src/main/java/com/example/octokitgithubprojects/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.octokitgithubprojects
+package com.example.octokitgithubprojects.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.octokitgithubprojects.R
 
 class MainActivity : AppCompatActivity() {
 
