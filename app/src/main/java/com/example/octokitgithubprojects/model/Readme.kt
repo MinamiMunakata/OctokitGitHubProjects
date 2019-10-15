2cec50a7153df0095ad4d7348ca5108dd91d4493
Minami Munakata <june28munazo@gmail.com>
2019-10-15 15:34:33 +0900

    Delete unused Model and ViewModel.

--- app/src/main/java/com/example/octokitgithubprojects/model/Readme.kt
+++
@@ -1,5 +0,0 @@
-package com.example.octokitgithubprojects.model
-
-data class Readme(var html: String) {
-
-}
--- app/src/main/java/com/example/octokitgithubprojects/repository/ProjectRepository.kt
+++ app/src/main/java/com/example/octokitgithubprojects/repository/ProjectRepository.kt
@@ -1,7 +1,6 @@
 package com.example.octokitgithubprojects.repository
 
 import com.example.octokitgithubprojects.model.Project
-import com.example.octokitgithubprojects.model.Readme
 import com.example.octokitgithubprojects.service.GitHubService
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -19,15 +18,14 @@ class ProjectRepository {
     private var gitHubService: GitHubService = retrofit.create(GitHubService::class.java)
 
     // HTTP request returns the list of repositories.
-    suspend fun getProjectList(userId: String): Response<List<Project>> = gitHubService.getProjectList(userId)
-
-    suspend fun getProjectREADME(userId: String, projectName: String): Response<Readme> = gitHubService.getReadme(userId, projectName)
+    suspend fun getProjectList(userId: String): Response<List<Project>> =
+        gitHubService.getProjectList(userId)
 
     // Return the singleton ProjectRepository instance.
     companion object Factory {
         val instance: ProjectRepository
-        @Synchronized get() {
-            return ProjectRepository()
-        }
+            @Synchronized get() {
+                return ProjectRepository()
+            }
     }
 }
